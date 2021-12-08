613e3ea9b74f4faff1b08662f653a1246793e5cf
Yousef Sayed <sayed.yousaf@gmail.com>
2021-12-08 12:03:38 +0000

    add images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 const App = () => {
   return (
@@ -10,9 +12,10 @@ const App = () => {
         <section>
           <h1>Hello React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword svg" width="250" />
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   );
 };
